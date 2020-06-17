1b4cdea2c9aa6ec825cd416dc33716994f22b631
julimancan <julian.m.bustos@gmail.com>
2020-06-17 17:58:18 -0400

    second push

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetDB = {
+    user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+    },
+    content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+};
+
+const createTweetElement = (tweetDB) => {
+    const $tweet = $("<article>").addClass("tweet");
+    const html = `<div>
+				<h4>${tweetDB.user.name}</h4>
+				<div class="id">${tweetDB.user.handle}</div>
+			</div>
+			<p class="gossipText">${tweetDB.content.text}</p>
+			<div class="footer">${tweetDB.created_at}
+				<div class="socials">L F R</div>
+			</div>`;
+    const tweetElement = $tweet.append(html);
+    return tweetElement;
+};
+// $($tweet).append(tweetDB);
+const $tweet = createTweetElement(tweetDB);
+
+
+$(document).ready(() => {
+    console.log($tweet);
+    $("#tweet-container").append($tweet);
+
+    $("#button").submit((event) => {
+        event.preventDefault();
+        console.log('prevented?')
+    })
+});
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -28,6 +29,10 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+
+
+
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
