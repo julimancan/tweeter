5448970e2334cbeca0b34cf8c0309fd374b188ba
julimancan <julian.m.bustos@gmail.com>
2020-06-17 21:10:06 -0400

    posts are posted and updated

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,42 +3,56 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const tweetDB = {
-    user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-    },
-    content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-};
-
-const createTweetElement = (tweetDB) => {
-    const $tweet = $("<article>").addClass("tweet");
-    const html = `<div>
-				<h4>${tweetDB.user.name}</h4>
-				<div class="id">${tweetDB.user.handle}</div>
-			</div>
-			<p class="gossipText">${tweetDB.content.text}</p>
-			<div class="footer">${tweetDB.created_at}
-				<div class="socials">L F R</div>
+const createTweetElement = (tweetObj) => {
+    console.log(tweetObj);
+    const $tweet = $("#tweet-container");
+    const html =
+        `<div class="tweet">
+            <section class="tweet-header">
+            <div class="avatar"><img src="${tweetObj.user.avatars}"><h4>${tweetObj.user.name}</h4></div>
+			<div class="id">${tweetObj.user.handle}</div>
+            </section>
+			<p class="gossipText">${tweetObj.content.text}</p>
+			<div class="footer">${tweetObj.created_at}
+            <div class="socials">L F R</div>
+            </div>
 			</div>`;
-    const tweetElement = $tweet.append(html);
-    return tweetElement;
+    $tweet.prepend(html);
+    // return tweetElement;
 };
-// $($tweet).append(tweetDB);
-const $tweet = createTweetElement(tweetDB);
 
+const renderTweets = function(tweetDB) {
+    for (let tweet of tweetDB) {
+        createTweetElement(tweet);
+    }
+};
+
+const loadTweets = function() {
+    $("#tweet-container").empty();
+    $.ajax("/tweets", {
+        method: "GET",
+    }).then(function(data) {
+        console.log("loaded the tweets");
+        renderTweets(data);
+    });
+};
 
 $(document).ready(() => {
-    console.log($tweet);
-    $("#tweet-container").append($tweet);
-
-    $("#button").submit((event) => {
+    loadTweets();
+    $("form").submit(function(event) {
         event.preventDefault();
-        console.log('prevented?')
-    })
+        console.log("prevented!");
+        console.log("serialized form", $("#form").serialize());
+        $.ajax({
+                method: "POST",
+                url: "/tweets",
+                data: $(this).serialize(),
+            })
+            .then(function(res) {
+                loadTweets();
+            })
+            .catch((err) => {
+                console.log("error", err);
+            });
+    });
 });
