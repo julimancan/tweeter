8eccd7ce41faccd7d582952bf23b2d754ed52932
julimancan <julian.m.bustos@gmail.com>
2020-06-18 17:08:24 -0400

    added comments to the important functions and removed commented out code and console.logs

--- public/scripts/composer-char.js
+++ public/scripts/composer-char.js
@@ -1,20 +1,19 @@
-$(document).ready(function () {
-  // --- our code goes here ---
+$(document).ready(function() {
+    // --- our code goes here ---
 
-  console.log('file loaded correctly');
-
-  $('#tweet-text').on('keyup', function () {
-    const tweetLength = $(this).val().length;
-    let charLeft = 140 - tweetLength;
-    const counter = $(this).closest('form').find('.counter')[0];
-    $(counter).val(charLeft);
-    if (charLeft > 0) {
-      $(counter).removeClass('negative');
-      $(counter).addClass('positive');
-    }
-    if (charLeft < 0) {
-      $(counter).removeClass('positive');
-      $(counter).addClass('negative');
-    }})
+    // counts the characters inputted and renders the remaining characters 
+    $('#tweet-text').on('keyup', function() {
+        const tweetLength = $(this).val().length;
+        let charLeft = 140 - tweetLength;
+        const counter = $(this).closest('form').find('.counter')[0];
+        $(counter).val(charLeft);
+        if (charLeft > 0) {
+            $(counter).removeClass('negative');
+            $(counter).addClass('positive');
+        }
+        if (charLeft < 0) {
+            $(counter).removeClass('positive');
+            $(counter).addClass('negative');
+        }
+    })
 });
-
